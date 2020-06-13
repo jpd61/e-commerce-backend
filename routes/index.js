528b9158be74ace430581de26e8e28cdf187d94a
jpd61 <jpd@dewoodyoil.com>
2020-06-13 12:55:27 -0500

    upload videos

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("Wrong Route!")
 });
 
 module.exports = router;
